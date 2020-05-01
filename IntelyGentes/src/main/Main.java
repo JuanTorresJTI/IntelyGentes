3e092e8a3bb30245ed7674a1e8e72b0029316630
JuanTorresJTI <juantorresjti@outlook.com>
2020-05-01 11:53:14 +0200

    Inicio del login

--- IntelyGentes/src/main/Main.java
+++ IntelyGentes/src/main/Main.java
@@ -3,8 +3,12 @@ package main;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		login();
+		
+	}
 
+	private static void login() {
+		
 	}
 
 }
