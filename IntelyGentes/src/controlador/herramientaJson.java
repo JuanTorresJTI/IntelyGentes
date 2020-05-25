ff03543e071bb0f402237d05c45b2db039928731
Juan Torres <46530979+JuanTorresJTI@users.noreply.github.com>
2020-05-25 21:22:48 +0200

    Producto final del programa

--- IntelyGentes/src/controlador/herramientaJson.java
+++ IntelyGentes/src/controlador/herramientaJson.java
@@ -4,11 +4,19 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.reflect.Type;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 import modelo.Usuario;
 
@@ -17,9 +25,10 @@ public class herramientaJson {
 	public static ArrayList<Usuario> lectorUsuarios() {
 		ArrayList<Usuario> usuarios = new ArrayList<Usuario>();
 		try {
-			FileReader fr = new FileReader("usuarios.json");
+			FileReader fr = new FileReader("usuario.json");
 			Gson gson = new Gson();
-			Type tipoListaUsuarios = new TypeToken<ArrayList<Usuario>>(){}.getType();
+			Type tipoListaUsuarios = new TypeToken<ArrayList<Usuario>>() {
+			}.getType();
 			usuarios = gson.fromJson(fr, tipoListaUsuarios);
 		} catch (Exception e) {
 			System.out.println("Fallo en: " + e);
@@ -27,28 +36,107 @@ public class herramientaJson {
 		}
 		return usuarios;
 	}
-	
-	public static Usuario getUsuario (String email) {
+
+	public static Usuario getUsuario(String email) {
 		ArrayList<Usuario> usuarios = lectorUsuarios();
 		Usuario u = new Usuario();
-		for (int i = 0 ; i < usuarios.size(); i++) {
+		for (int i = 0; i < usuarios.size(); i++) {
 			u = usuarios.get(i);
 			if (u.getEmail().equalsIgnoreCase(email)) {
-				return u ;
+				return u;
 			}
 		}
-		return null;
+		return u;
 	}
 
 	public static void guardarUsuarios(Usuario usuarioNuevo) {
 		ArrayList<Usuario> usuarios = lectorUsuarios();
 		usuarios.add(usuarioNuevo);
 		Gson gson = new GsonBuilder().setPrettyPrinting().create();
-		try (FileWriter writer = new FileWriter("usuarios.json")){
+		try (FileWriter writer = new FileWriter("usuario.json")) {
 			gson.toJson(usuarios, writer);
-		}catch (IOException e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
+	public static void escribirFicherCSV(String usuario) {
+		String nombreProducto, descripcion, tipoPrenda, marca, precio;
+		int id = (int) Math.random() * 1000000;
+
+		Scanner in = new Scanner(System.in);
+		System.out.println("Introduce los valores de tu producto");
+
+		System.out.println("------------------------------------");
+		System.out.print("Nombre del Producto: ");
+		nombreProducto = in.nextLine();
+
+		System.out.print("Marca: ");
+		marca = in.nextLine();
+
+		System.out.print("Tipo de Prenda: ");
+		tipoPrenda = in.nextLine();
+
+		System.out.println("Descripción del producto: ");
+		descripcion = in.nextLine();
+
+		System.out.print("Precio del producto: ");
+		precio = in.nextLine();
+		System.out.println("------------------------------------");
+
+		File tmpDir = new File("./productos");
+		String fileName = "productos";
+		if (tmpDir.exists()) {
+			try (FileWriter writer = new FileWriter(fileName, true)) {
+				writer.append(Integer.toString(id) + ", " + nombreProducto + ", " + descripcion + ", " + tipoPrenda
+						+ ", " + marca + ", " + usuario + ", " + precio);
+				writer.append(System.lineSeparator());
+				writer.flush();
+				writer.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} else {
+			try (FileWriter writer = new FileWriter(fileName, true)) {
+				writer.append("ID, NombreProducto, Descripción, TipoPrenda, Marca, NombreVendedor, Precio");
+				writer.append(System.lineSeparator());
+				writer.append(Integer.toString(id) + ", " + nombreProducto + ", " + descripcion + ", " + tipoPrenda
+						+ ", " + marca + ", " + usuario + ", " + precio);
+				writer.append(System.lineSeparator());
+
+				writer.flush();
+				writer.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static void mostrarProductos() {
+		List<String[]> productos = leer(",", "productos");
+		System.out.println("*******************************************************************");
+		for (String[] s : productos) {
+			System.out.print("|");
+			for (String p: s) {
+				System.out.print(p + " | ");
+			}
+			System.out.println();
+		}
+		System.out.println("*******************************************************************");
+	}
+
+	public static List<String[]> leer(String separator, String fileName) {
+		try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+			List<String[]> list = new ArrayList<>();
+			String line = "";
+			while ((line = reader.readLine()) != null) {
+				String[] array = line.split(separator);
+				list.add(array);
+			}
+			return list;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 }
--- IntelyGentes/src/main/Main.java
+++ IntelyGentes/src/main/Main.java
@@ -2,6 +2,7 @@ package main;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -11,154 +12,176 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import modelo.Productos;
 import java.util.Scanner;
 
+import controlador.herramientaJson;
 import modelo.Usuario;
 
 public class Main {
-
 	public static void main(String[] args) {
-
-		List<String[]> productos = new ArrayList<String[]>();
-		String[] producto = new String[7];
-		for (int i = 0; i < producto.length; i++) {
-			producto[i] = Integer.toString(i + 2);
-		}
-		productos.add(producto);
-		escribir(productos, ",", "productos");
-
-		List<String[]> productos1 = leer(",", "productos");
-
-		for (String[] strings : productos1) {
-			for (String strings2 : strings) {
-				System.out.print(strings2 + " ");
-			}
-		}
 		login();
 	}
+
 	private static void login() {
-		//Constructor:
-		Scanner teclado = new Scanner(System.in);
-		System.out.println("Bienvenido a la aplicación IntelYGentes" ); 
-		
-		boolean repetir=true;
-		while(repetir=true) {
-			System.out.println("¿Qué operación desea realizar? " +"\n"+"\t" +
-					"1.Iniciar sesión"+ "\n"+"\t"+
-					"2.Registrarse" +"\n"+ "\t"+  
-					"3.Salir"+ "\n"+"\t");
-					String respuesta = teclado.nextLine();
-		switch (respuesta){
+		Scanner in = new Scanner(System.in);
+		System.out.println("***************************************");
+		System.out.println("Bienvenido a la aplicación IntelYGentes");
+		System.out.println("***************************************");
+
+		boolean repetir = true;
+		while (repetir) {
+			
+			System.out.println(
+					"Qué operación desea realizar? " + "\n\t1.Iniciar sesión" + "\n\t2.Registrarse" + "\n\t3.Salir");
+			String respuesta = in.nextLine();
+			switch (respuesta) {
 			case "1":
 				validarUsuario();
-				repetir=false;
-			break;
+				repetir = false;
+				break;
 			case "2":
 				preguntas();
-				repetir=false;
-			break;
+				break;
 			case "3":
 				System.out.println("Has solicitado salir. ¡Hasta pronto!");
-				repetir=false;
-				//System.exit(0);
+				repetir = false;
+				break;
 			default:
 				System.out.println("Has introducido algo incorrecto");
 				System.out.println("Vuelva a introducir una opción:");
 				break;
-		}
-		}
-		
-		//dni telefono contraseña mail
-}
-	public static void escribir(List<String[]> thingsToWrite, String separator, String fileName) {
-		try (FileWriter writer = new FileWriter(fileName, true)) {
-			for (String[] strings : thingsToWrite) {
-				for (int i = 0; i < strings.length; i++) {
-					writer.append(strings[i]);
-					if (i < (strings.length - 1))
-						writer.append(separator);
-				}
-				writer.append(System.lineSeparator());
-
-
 			}
-			writer.flush();
-			writer.close();
-		} catch (IOException e) {
-			e.printStackTrace();
 		}
 	}
+
 	public static void preguntas() {
 		Scanner teclado = new Scanner(System.in);
 		System.out.println("Ha solicitado registrarse. ");
 		System.out.print("\nIntroduzca su nombre: ");
-		String nombre=teclado.nextLine();
+		String nombre = teclado.nextLine();
 		System.out.print("\nIntroduzca su apellido: ");
-		String apellido =teclado.nextLine();
+		String apellido = teclado.nextLine();
 		System.out.print("\nIntroduzca su DNI: ");
-		String dni= teclado.nextLine();
+		String dni = teclado.nextLine();
 		System.out.print("\nIntroduzca su número de teléfono: ");
-		String telefono= teclado.nextLine();
+		String telefono = teclado.nextLine();
 		System.out.print("\nIntroduzca su correo electrónico: ");
-		String mail= teclado.nextLine();
+		String mail = teclado.nextLine();
 		System.out.print("\nIntroduzca una constraseña para su cuenta: ");
-		String contrasena= teclado.nextLine();
+		String contrasena = teclado.nextLine();
 		System.out.println("Ha creado una cuenta correctamente.");
-		
-		Usuario usuario1=new Usuario(dni, nombre, apellido, telefono, contrasena, mail, false);
-		//guardarUsuarios(usuario1);
+
+		Usuario usuario1 = new Usuario(dni, nombre, apellido, telefono, contrasena, mail, false);
+		herramientaJson.guardarUsuarios(usuario1);
 	}
+
 	public static void validarUsuario() {
-		Scanner teclado = new Scanner(System.in);
+		Usuario user;
+		Scanner in = new Scanner(System.in);
+		boolean salir = true;
 		System.out.println("Ha solicitado iniciar sesión");
-		System.out.print("\nIntroduzca su correo electrónico: ");
-		String correoUsuario= teclado.nextLine();
-		System.out.print("\nIntroduzca su contraseña: ");
-		String contrasenaUsuario= teclado.nextLine();
-		
+
+		do {
+			System.out.println("Introduzca su correo electrónico: ");
+			String correoUsuario = in.nextLine();
+
+			System.out.println("Introduzca su contraseña: ");
+			String contrasenaUsuario = in.nextLine();
+
+			user = herramientaJson.getUsuario(correoUsuario);
+			if (user.getContrasena().equals(contrasenaUsuario) && user != null) {
+				System.out.println("Bienvenido");
+				if (user.isSuperuser()) {
+					ventanaSuperUser(user.getNombre(), user.getApellidos());
+				} else {
+					ventanaUser(user.getNombre(), user.getApellidos());
+				}
+				salir = false;
+			} else {
+				System.out.println("Usuario o contraseña incorrecta");
+			}
+		} while (salir);
+
 	}
 
+	private static void ventanaSuperUser(String nombre, String apellidos) {
+		Scanner in = new Scanner(System.in);
+		ArrayList<Usuario> usuarios;
+		boolean salir = true;
+		System.out.println("¡Bienvenido " + nombre + " " + apellidos + "!");
+		do {
+			System.out.println("---------------------------------------------------------");
+			System.out.println("Escoge una opcion (1-3): ");
+			System.out.println("\t1) Mostrar Productos");
+			System.out.println("\t2) Mostrar Usuarios");
+			System.out.println("\t3) Salir");
+			System.out.println("---------------------------------------------------------");
 
-	public void escribirFicher(){
-		try {
-			// create a list of objects
-			List<List<String>> records = Arrays.asList(Arrays.asList("1", "John Lee", "US"),
-					Arrays.asList("2", "Jovan Roover", "DE"), Arrays.asList("3", "Emma Watson", "UK"));
-
-			// create a writer
-			BufferedWriter writer = Files.newBufferedWriter(Paths.get("productos.csv"));
-
-			// write header record
-			writer.write("ID,Name,Country");
-			writer.newLine();
-
-			// write all records
-			for (List<String> record : records) {
-				writer.write(String.join(",", record));
-				writer.newLine();
+			String respuesta = in.nextLine();
+			switch (respuesta) {
+			case "1":
+				herramientaJson.mostrarProductos();
+				break;
+			case "2":
+				usuarios = herramientaJson.lectorUsuarios();
+				mostarUsers(usuarios);
+				break;
+			case "3":
+				System.out.println("¡Hasta pronto!");
+				salir = false;
+				break;
+			default:
+				System.out.println("Has introducido algo incorrecto");
+				System.out.println("Vuelva a introducir una opción:");
+				break;
 			}
+		} while (salir);
+	}
 
-			// close the writer
-			writer.close();
-
-		} catch (IOException ex) {
-			ex.printStackTrace();
+	private static void mostarUsers(ArrayList<Usuario> usuarios) {
+		System.out.println("Lista de vendedores");
+		for (Usuario usuario : usuarios) {
+			if (!usuario.isSuperuser()) {
+				System.out.println("---------------------------------------------------------");
+				System.out.println("Nombre: " + usuario.getNombre());
+				System.out.println("Apellidos: " + usuario.getApellidos());
+				System.out.println("Email: " + usuario.getEmail());
+				System.out.println("Teléfono" + usuario.getTelefono());
+				System.out.println("---------------------------------------------------------");
+			}
 		}
 	}
-	// Allows to define custom separator
-	public static List<String[]> leer(String separator, String fileName) {
-		try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
-			List<String[]> list = new ArrayList<>();
-			String line = "";
-			while ((line = reader.readLine()) != null) {
-				String[] array = line.split(separator);
-				list.add(array);
+
+	private static void ventanaUser(String nombre, String apellidos) {
+		Scanner in = new Scanner(System.in);
+		boolean salir = true;
+		System.out.println("¡Bienvenido " + nombre + " " + apellidos + "!");
+		do {
+			System.out.println("---------------------------------------------------------");
+			System.out.println("Escoge una opcion (1-3): ");
+			System.out.println("\t1) Mostrar Productos");
+			System.out.println("\t2) Meter Producto");
+			System.out.println("\t3) Salir");
+			System.out.println("---------------------------------------------------------");
+
+			String respuesta = in.nextLine();
+			switch (respuesta) {
+			case "1":
+				herramientaJson.mostrarProductos();
+				break;
+			case "2":
+				herramientaJson.escribirFicherCSV(nombre + " " + apellidos);
+				break;
+			case "3":
+				System.out.println("¡Hasta pronto!");
+				salir = false;
+				break;
+			default:
+				System.out.println("Has introducido algo incorrecto");
+				System.out.println("Vuelva a introducir una opción:");
+				break;
 			}
-			return list;
-		} catch (IOException e) {
-			e.printStackTrace();
-			return null;
-		}
+		} while (salir);
 	}
+
 }
